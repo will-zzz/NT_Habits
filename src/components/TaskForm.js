bae07705f3afb4bfeb3392aab11630881ff2297e
Will Zakielarz <will.zakielarz@duke.edu>
2023-07-24 18:24:58 -0400

    mfghhhh its workinguhhhhhhh

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,30 +1,51 @@
 import React, { useState } from "react";
-import { auth, database } from "../firebase/firebaseConfig";
+import { database } from "../firebase/firebaseConfig";
+import { ref, push, set } from "firebase/database";
 
-const TaskForm = ({ userId }) => {
-  const [taskName, setTaskName] = useState("");
+const TaskForm = ({ user }) => {
+  const [task, setTask] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (taskName.trim() !== "") {
-      const tasksRef = database.ref(`users/${userId}/tasks`);
-      tasksRef.push({ name: taskName, completed: false });
-      setTaskName("");
+
+    // Make sure the task field is not empty before submitting
+    if (task.trim() === "") {
+      return;
     }
+
+    // Create a new task object with the task data
+    const newTask = {
+      task: task.trim(),
+    };
+
+    // Push the new task to the user's tasks node in Firebase database
+    // database.ref(`users/${userId}/tasks`).push(newTask);
+    const taskListRef = ref(database, `users/${user.uid}/tasks`);
+    const newTaskListRef = push(taskListRef);
+    set(newTaskListRef, { task: newTask.task, completed: false });
+
+    // Clear the task field after submission
+    setTask("");
   };
 
   return (
-    <div className="my-4">
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-        onClick={() => {} /* Add code to show the task form */}
-      >
-        Add Task
-      </button>
-
-      {/* Add code for the task form */}
-      {/* Include a text input and a submit button */}
-    </div>
+    <form onSubmit={handleSubmit} className="mt-4">
+      <div className="flex">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter your task"
+          className="mr-2 px-4 py-2 border rounded-lg"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+        >
+          Add Task
+        </button>
+      </div>
+    </form>
   );
 };
 
